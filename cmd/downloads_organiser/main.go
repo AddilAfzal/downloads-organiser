06a98466a3ad3d0cd366c58625a9c53f5af47b19
AddilAfzal <>
2022-09-15 22:07:04 +0100

    Replace logging

--- cmd/downloads_organiser/main.go
+++ cmd/downloads_organiser/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"log"
 	"path"
 	"strings"
@@ -13,15 +14,19 @@ import (
 
 func main() {
 	config.Init()
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors:   true,
+		DisableColors: false,
+	})
 
 	// Make the channel buffered to ensure no event is dropped. Notify will drop
 	// an event if the receiver is not able to keep up the sending pace.
 	c := make(chan notify.EventInfo, 1000)
 
 	watchFolder := fmt.Sprintf("%s/...", config.DownloadsFolder)
-	log.Printf("Watching `%s`", watchFolder)
+	logrus.Infof("Watching `%s`", watchFolder)
 	if err := notify.Watch(watchFolder, c, notify.InCloseWrite, notify.InMovedTo, notify.InMoveSelf, notify.InCloseNowrite); err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 	defer notify.Stop(c)
 
@@ -29,11 +34,11 @@ func main() {
 		// Block until an event is received.
 		switch ei := <-c; ei.Event() {
 		case notify.InCloseWrite:
-			log.Println("File", ei.Path(), "was written to in the watched directory.")
+			logrus.Info("File", ei.Path(), "was written to in the watched directory.")
 			handleFile(ei)
 			continue
 		case notify.InMovedTo:
-			log.Println("File", ei.Path(), "was swapped/moved into the watched directory.")
+			logrus.Info("File", ei.Path(), "was swapped/moved into the watched directory.")
 			handleFile(ei)
 			continue
 		default:
@@ -81,9 +86,9 @@ func handleFile(ei notify.EventInfo) {
 			}
 			HandleMovie(m)
 		} else {
-			log.Printf("What is this file? %s \n", fileName)
+			logrus.Warnf("What is this file? %s \n", fileName)
 		}
 	} else {
-		log.Print("Not .mkv file, skipping")
+		logrus.Info("Not .mkv file, skipping")
 	}
 }
--- internal/downloads_organiser/asset_handlers.go
+++ internal/downloads_organiser/asset_handlers.go
@@ -2,6 +2,7 @@ package downloads_organiser
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"log"
 	"os"
 	"path"
@@ -12,24 +13,24 @@ import (
 func HandleShow(show *TVShow) {
 	showFolder := fmt.Sprintf("%s/%s", config.ShowsFolder, show.Name)
 	if _, err := os.Stat(showFolder); os.IsNotExist(err) {
-		log.Printf("Folder \"%s\" does not exist... creating \n", show.Name)
+		logrus.Infof("Folder \"%s\" does not exist... creating \n", show.Name)
 		err = os.Mkdir(showFolder, os.ModePerm)
 		if err != nil {
-			log.Printf("Failed to create directory. %s", err)
+			logrus.Warnf("Failed to create directory. %s", err)
 		}
 	}
 
 	showSeasonFolder := fmt.Sprintf("%s/%s", showFolder, show.Season)
 	if _, err := os.Stat(showSeasonFolder); os.IsNotExist(err) {
-		log.Printf("Folder \"%s\" does not exist... creating \n", show.Season)
+		logrus.Infof("Folder \"%s\" does not exist... creating \n", show.Season)
 		err = os.Mkdir(showSeasonFolder, os.ModePerm)
 		if err != nil {
-			log.Printf("Failed to create directory. %s", err)
+			logrus.Warnf("Failed to create directory. %s", err)
 		}
 	}
 
 	newPath := fmt.Sprintf("%s/%s", showSeasonFolder, show.FileName)
-	log.Printf("Moving file to %s", newPath)
+	logrus.Infof("Moving file to %s", newPath)
 	err := MoveFile(show.FilePath, newPath)
 	if err != nil {
 		fmt.Println(err)
@@ -38,7 +39,7 @@ func HandleShow(show *TVShow) {
 
 func HandleMovie(movie *Movie) {
 	newPath := fmt.Sprintf("%s/%s", config.MoviesFolder, movie.FileName)
-	log.Printf("Moving file to %s", newPath)
+	logrus.Infof("Moving file to %s", newPath)
 	err := MoveFile(movie.FilePath, newPath)
 	if err != nil {
 		fmt.Println(err)
@@ -53,7 +54,7 @@ func HandleMovie(movie *Movie) {
 
 	// Check if empty
 	if res, _ := IsEmpty(fileFolder); res {
-		log.Printf("We can delete %s", fileFolder)
+		logrus.Infof("We can delete %s", fileFolder)
 		err = os.Remove(fileFolder)
 		if err != nil {
 			log.Print(err)
